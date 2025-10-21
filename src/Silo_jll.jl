e875541ea578bbced38c06ed8faae54b7b43ec11
jlbuild <juliabuildbot@gmail.com>
2025-10-21 20:01:08 +0000

    Silo_jll build 4.11.1+0

---
+++ src/Silo_jll.jl
@@ -0,0 +1,10 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule Silo_jll
+using Base
+using Base: UUID
+using MPIPreferences
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("Silo")
+JLLWrappers.@generate_main_file("Silo", UUID("f5d339c9-093b-5756-8f08-ceae06537255"))
+end  # module Silo_jll
